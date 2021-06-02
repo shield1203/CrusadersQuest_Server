36d2eaf27f565b41ea5ffdbd306beb4c9ab1ae55
KimJiHo <shield1203@naver.com>
2021-06-03 04:58:17 +0900

    UserData 송, 수신

--- router/main.js
+++ router/main.js
@@ -13,6 +13,14 @@ module.exports = function(app){
 
     /// Router Module ///
 
+    const UserData = function(req, res){
+        const sql = 'SELECT lv, exp, name, honor, diamond, gold, meat FROM user WHERE user_id=?';
+        const params = [req.query.userId];
+
+        console.log('Request[UserData] : ' + req.query.userId);
+        SendUserData(res, sql, params);
+    }
+
     const SoldierList = function(req, res){
         const sql = 'SELECT * FROM soldier WHERE fk_user_id=?';
         const params = [req.query.userId];
@@ -25,36 +33,54 @@ module.exports = function(app){
         const sql = 'UPDATE soldier SET team=? WHERE soldier_id=? AND fk_user_id=?';
         const params = [req.query.isTeam, req.query.soldierId, req.query.userId];
 
+        console.log('Request[UpdateTeam] : ' + req.query.userId);
+        SendUpdateTeamResult(res, sql, params);
+    }
+
+    /// Send Module ///
+    
+    const SendUserData = function(res, sql, params){
+        con.query(sql, params, function (error, results, fields) {
+            if(results == ''){
+                console.log('Result[UserData] : fail');
+                res.send('fail');
+            }
+            else{     
+                console.log('Result[UserData] : success');
+                res.send(results);
+            }
+        });
+    }
+
+    const SendSoldierList = function(res, sql, params){
+        con.query(sql, params, function (error, results, fields) {
+            if(results == ''){
+                console.log('Result[SoldierList] : fail');
+                res.send('fail');
+            }
+            else{     
+                console.log('Result[SoldierList] : success');
+                res.send(results);
+            }
+        });
+    }
+
+    const SendUpdateTeamResult = function(res, sql, params){
         con.query(sql, params, function (error, results, fields){
             if(error){
                 console.log(error);
                 res.send('fail');
             }
             else{     
-                console.log('Request[UpdateTeam] : ' + req.query.userId);
+                console.log('Result[UpdateTeam] : success');
                 res.send('success');
-            }
-        });
-    }
-
-    /// Send Module ///
-
-    const SendSoldierList = function(res, sql, params){
-        con.query(sql, params, function (error, results, fields) {
-            if(results == ''){
-                console.log('Result[Check] : fail');
-                res.send('fail');
-            }
-            else{     
-                console.log('Result[Check] : success');
-                console.log(results);
-                res.send(results);
             }
         });
     }
 
     /// GET, POST ///
     
+    app.get('/UserData', UserData);
     app.get('/SoldierList', SoldierList);
     app.get('/UpdateTeam', UpdateTeam);
 }
